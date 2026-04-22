89eae5a4423a7ed688911c9e1020d50cf14b2c9e
giji676 <tvtengoxachidze@gmail.com>
2026-04-22 21:02:10 +0100

    Changed test calling

--- src/test/test.c
+++ src/test/test.c
@@ -1,47 +1,20 @@
-#include "formats/bmp.h"
-#include "formats/png.h"
-#include "common/common.h"
 #include "test/display/display.h"
 #include "gj_image/gj_image.h"
 #include <stdio.h>
 
-int test_bmp(const char *filename) {
+int test_image(const char *filename) {
     int width, height, channels;
-    struct image_file image_file = {
-        .filename = filename,
-        .width = &width,
-        .height = &height,
-        .channels = &channels
-    };
-    unsigned char *data = bmp_open(&image_file);
-    if (!data) {
-        printf("Err: %s", gj_get_last_error());
-        return -1;
-    }
-    display_image(data, *image_file.width, *image_file.height, *image_file.channels);
-    gj_image_free(data);
-    return 0;
-}
-
-int test_png(const char *filename) {
-    int width, height, channels;
-    struct image_file image_file = {
-        .filename = filename,
-        .width = &width,
-        .height = &height,
-        .channels = &channels
-    };
-    unsigned char *data = png_open(&image_file);
+    unsigned char *data = gj_image_load(filename, &width, &height, &channels);
     if (!data) {
         printf("Err: %s", gj_get_last_error());
         return -1; }
-    display_image(data, *image_file.width, *image_file.height, *image_file.channels);
+    display_image(data, width, height, channels);
     gj_image_free(data);
     return 0;
 }
 
 int main() {
-    test_bmp("assets/anaconda.bmp");
-    test_png("assets/ntxr001.png");
+    test_image("assets/anaconda.bmp");
+    test_image("assets/ntxr001.png");
     return 0;
 }
